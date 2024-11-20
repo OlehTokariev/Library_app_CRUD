3c41692665c54f28315f3c503645fe5bed60d502
Oleh Tokariev <tokarevolega95@gmail.com>
2024-11-20 13:13:54 +0200

    Single resp. services/ added id for author, Validation in controllers/ removed extra files, code

--- src/main/java/library/example/libraryEdu/service/AuthorService.java
+++ src/main/java/library/example/libraryEdu/service/AuthorService.java
@@ -7,7 +7,6 @@ import library.example.libraryEdu.model.Author;
 import library.example.libraryEdu.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
