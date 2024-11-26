47e15cb207163f592912527a10c35bbeaea3d596
Oleh Tokariev <tokarevolega95@gmail.com>
2024-11-26 11:11:19 +0200

    Added id validation in AuthorDTO

--- src/main/java/library/example/libraryEdu/dto/AuthorDTO.java
+++ src/main/java/library/example/libraryEdu/dto/AuthorDTO.java
@@ -1,5 +1,6 @@
 package library.example.libraryEdu.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthorDTO {
+    @Min(value = 1, message = "ID must be greater than or equal to 1")
     private Long id;
 
     @NotBlank(message = "Firstname must not be blank")
